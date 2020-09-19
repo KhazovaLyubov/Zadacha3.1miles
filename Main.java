4b1025c1a5cc201dffc77d2fab64693e10c9fc50
Khazova Lyubov <lyubashilina@rambler.ru>
2020-09-19 22:40:35 +0400

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20;
+        int sum = 750050;
+        int mile = sum/(amount*100);
+
+        System.out.println(mile);
+    }
+}
